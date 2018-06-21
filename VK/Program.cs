f02cc1f8641ad1c91147f9a26c3a900cf5e34e14
MrAndrejXz <A-777.77@mail.ru>
2018-06-21 14:27:06 +0700

    Добавлены комментарии

--- VK/Program.cs
+++ VK/Program.cs
@@ -17,6 +17,9 @@ namespace VK
         static token_struct token;
         static group_struct group;
 
+        /// <summary>
+        /// Структура, хранящая ID групп
+        /// </summary>
         [Serializable]
         public struct group_struct
         {
@@ -24,6 +27,9 @@ namespace VK
             public string groupID_copy;
         }
 
+        /// <summary>
+        /// Структура, хранящая токены
+        /// </summary>
         [Serializable]
         public struct token_struct
         {
@@ -31,6 +37,9 @@ namespace VK
             public string tokenGroup;
         }
 
+        /// <summary>
+        /// Структура, хранящая смещения
+        /// </summary>
         [Serializable]
         public struct offset_struct
         {
@@ -47,6 +56,13 @@ namespace VK
             Console.ReadLine();
         }
 
+        /// <summary>
+        /// Выгружает строку на указаный ресурс
+        /// </summary>
+        /// <param name="req">URL ресурса</param>
+        /// <param name="metod">Метод передачи</param>
+        /// <param name="data">Выгружаемая строка</param>
+        /// <returns></returns>
         static string webRequestString(string req, string metod, string data)
         {
             string response;
@@ -57,6 +73,12 @@ namespace VK
             return response;
         }
 
+        /// Выгружает файл на указаный ресурс
+        /// </summary>
+        /// <param name="req">URL ресурса</param>
+        /// <param name="metod">Метод передачи</param>
+        /// <param name="data">Выгружаемая строка</param>
+        /// <returns></returns>
         static string webRequestFile(string req,string metod, string filepath)
         {
             string response;
@@ -67,67 +89,74 @@ namespace VK
             return response;
         }
 
+
         static void getAlbums()
         {
             int index = 0;
-            var req = "https://api.vk.com/method/photos.getAlbums";
-            var data = "&owner_id=" + group.groupID_source + "&v=" + version + "&access_token=" + token.tokenApp;
-            var rez = webRequestString(req, "POST", data);
-            dynamic obj = jSS.DeserializeObject(rez);
+            //Получаем список альбомов
+            dynamic obj = jSS.DeserializeObject(webRequestString("https://api.vk.com/method/photos.getAlbums", "POST", "&owner_id=" + group.groupID_source + "&v=" + version + "&access_token=" + token.tokenApp));
             Console.WriteLine("Найдено объектов: " + obj["response"]["count"]);
-            var t = obj["response"]["items"][0];
-            /*!!!!!!!!!!!!!!!!!!!!!С конца и удаление файла!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+            //Идём по альбомам с конца
             for (int i = obj["response"]["count"]-2; i >= 0;i--)
             {
+                //Выводим текущий
                 Console.WriteLine(++index + ".\t" + obj["response"]["items"][i]["size"] + "\t" + obj["response"]["items"][i]["title"]);
+                //Проверяем, если его скачивали, то переходим к следующему
                 if (offset.offsetGroup >= index) continue;
+                //Сохраняем альбом
                 saveAlbum(obj["response"]["items"][i]);
                 offset.offsetGroup++;
                 offset.offsetPhoto = 0;
                 File.WriteAllText(@"Settings\offset.txt", jSS.Serialize(offset));
-                
             }
         }
 
+        /// <summary>
+        /// Создание альбома
+        /// </summary>
+        /// <param name="title">Название альбома</param>
         static string createAlbum(string title)
         {
-            var req = "https://api.vk.com/method/photos.createAlbum";
-            var data = "&upload_by_admins_only=1"+"&title=" + title + "&group_id=" + group.groupID_copy+ "&v=" + version + "&access_token=" + token.tokenApp;
-            dynamic rez = jSS.DeserializeObject(webRequestString(req, "POST", data));
+            dynamic rez = jSS.DeserializeObject(webRequestString("https://api.vk.com/method/photos.createAlbum", "POST", "&upload_by_admins_only=1" + "&title=" + title + "&group_id=" + group.groupID_copy + "&v=" + version + "&access_token=" + token.tokenApp));
             return rez["response"]["id"].ToString();
         }
 
+        /// <summary>
+        /// Загрузка фото в альбом
+        /// </summary>
+        /// <param name="albumID">id альбома</param>
+        /// <param name="path">Путь к файлу</param>
         static void uploadToVk(string albumID,string path)
         {
-            var req = "https://api.vk.com/method/photos.getUploadServer";
-            var data = "&album_id=" + albumID + "&group_id=" + group.groupID_copy + "&v=" + version + "&access_token=" + token.tokenApp;
             //Получаем сервер
-            dynamic serverResponse = jSS.DeserializeObject(webRequestString(req, "POST", data));
+            dynamic serverResponse = jSS.DeserializeObject(webRequestString("https://api.vk.com/method/photos.getUploadServer", "POST", "&album_id=" + albumID + "&group_id=" + group.groupID_copy + "&v=" + version + "&access_token=" + token.tokenApp));
             //Загружаем на сервер
             serverResponse = jSS.DeserializeObject(webRequestFile(serverResponse["response"]["upload_url"],"POST", path));
             //Сохраняем
-            req = "https://api.vk.com/method/photos.save";
-            data = "&album_id=" + albumID + "&group_id=" + group.groupID_copy + "&photos_list=" + serverResponse["photos_list"] + "&server="+serverResponse["server"]+"&hash="+serverResponse["hash"]+"&access_token="+token.tokenApp+"&v="+version;
-            var x=webRequestString(req, "POST", data);
+            var x=webRequestString("https://api.vk.com/method/photos.save", "POST", "&album_id=" + albumID + "&group_id=" + group.groupID_copy + "&photos_list=" + serverResponse["photos_list"] + "&server="+serverResponse["server"]+"&hash="+serverResponse["hash"]+"&access_token="+token.tokenApp+"&v="+version);
         }
 
+        /// <summary>
+        /// Сохранение альбома
+        /// </summary>
+        /// <param name="obj"></param>
         static void saveAlbum(dynamic obj)
         {
             int count = offset.offsetPhoto;
             string path = obj["title"];
-            var req = "https://api.vk.com/method/photos.get";
-            var data = "&offset="+offset.offsetPhoto+"&owner_id=" + obj["owner_id"] + "&album_id=" + obj["id"] + "&v=" + version + "&access_token=" + token.tokenApp;
-            var rez = webRequestString(req, "POST", data);
             string albumID;
+            //Создавали альбом
             if (offset.offsetPhoto == 0)
             {
+                //Создаем альбом
                 albumID = createAlbum(path);
                 offset.albumID = albumID;
             }
             else
                 albumID = offset.albumID;
-            obj = jSS.DeserializeObject(rez);
-
+            obj = jSS.DeserializeObject(webRequestString("https://api.vk.com/method/photos.get", "POST", "&offset=" + offset.offsetPhoto + "&owner_id=" + obj["owner_id"] + "&album_id=" + obj["id"] + "&v=" + version + "&access_token=" + token.tokenApp));
+            
+            //Поиск картинки наибольшего размера
             string url="";
             int width=0;
             int height=0;
@@ -145,10 +174,13 @@ namespace VK
                         url = itemSize["url"];
                     }
                 }
+                //Скачиваем фотографию
                 using (WebClient client = new WebClient())
                 {
                     try { 
+                        //Скачиваем
                         client.DownloadFile(new Uri(url), @"Foto\"+item["id"]+".jpg");
+                        //Загружаем в новый альбом
                         uploadToVk(albumID, @"Foto\" + item["id"] + ".jpg");
                         File.Delete(@"Foto\" + item["id"] + ".jpg");
                     }
@@ -163,14 +195,20 @@ namespace VK
             }
         }
 
+        /// <summary>
+        /// Печать на предыдущей строке
+        /// </summary>
+        /// <param name="txt">Текст печати</param>
         static void updateConsole(string txt)
         {
             var y = Console.CursorTop;
             Console.WriteLine(txt);
             Console.SetCursorPosition(0, y);
         }
+
         static void Func()
         {
+            //Инициализация
             group=jSS.Deserialize<group_struct>(File.ReadAllText(@"Settings\group"));
             token = jSS.Deserialize<token_struct>(File.ReadAllText(@"Settings\token"));
             offset = jSS.Deserialize<offset_struct>(File.ReadAllText(@"Settings\offset"));
